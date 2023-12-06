32f81bea182ac58133bac77e1ce572a7d7bccf43
JhonathanRamos <ramos.jhonathan.951@gmail.com>
2023-12-05 23:00:57 -0400

    java clase 8

--- holamundo1/src/holamundo1/Holamundo1.java
+++ holamundo1/src/holamundo1/Holamundo1.java
@@ -3,7 +3,19 @@ package holamundo1;
 public class Holamundo1 {
 
     public static void main(String[] args) {
-        System.err.println("hola Mundos");
+        System.out.println("hola Mundos");
+        
+        int a = 10;
+        System.out.println(a);
+        
+        a = 5;
+        System.out.println(a);
+        
+        String cadena = "saludos";
+        System.out.println(cadena);
+        
+        cadena = "Adios";
+        System.out.println(cadena);
     }
     
 }
